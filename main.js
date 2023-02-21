a682a7a00e651edf2692b51ea18ae2c6e3a0c135
Shani Victoria <122415617+Shaniivictoria@users.noreply.github.com>
2023-02-21 11:45:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// create two player 
+    currentPlayer = ""
+        let characters = {
+
+    player1:{
+        health: 5,
+        
+
+
+    }
+
+
+
+
+
+
+        }
+    
+
+//create 4 zoombies 
+
+
+//player one gets paint gun (blue)
+
+//player one aims at zoombie and throw paint on zoombie
+
+//zoombie melts (steam comes up)
+
+//player two gets pain gun (red)
+
+//player two aims at zoombies and throws paint 
+
+// zoombie melts 
+
+
+
+
+//round one = 1300 point 
+//onclick = paint spattered. 
+
