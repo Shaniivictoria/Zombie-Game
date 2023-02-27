f00a3d0f001736d84b5038d1743578015532b55d
Shani Victoria <122415617+Shaniivictoria@users.noreply.github.com>
2023-02-26 19:22:35 -0500

    UPDATED JS
    I HAVE REMOVED SOME FUNCTIONS AND ADDED NEW ONES. 
     I CREATED A ARRARY AND EVENT LISTENER TO SHOOT THE ZOMBIES AND FOR THE BUTTON TO WORK ON START.

--- main.js
+++ main.js
@@ -1,33 +1,116 @@
-const gameFrame = document.querySelector("#gameFrame");
-const button = document.querySelector(".button");
+var healthPoints = 100;
 
-const zombies = document.createElement("zombie");
-zombies.setAttribute("class", zombies);
+function updateScoreBar(points) {
 
+	scoorePoints = points;
+	var scoreBar = document.querySelector(".scoreBar");
+	scoreBar.style.width = points + "%";
 
-
-const gameFrameHeight = gameFrame.offsetHeight;
-const gameFrameWidth = gameFrame.offsetWidth;
-
-setInterval(() => {
-    const randTop = Math.random() * (contHeight - 100);
-    const randLeft = Math.random() * (contWidth - 100);
-
-    zombies.style.position = "absolute";
-    zombies.style.top = randTop + "px";
-    zombies.style.position = randLeft + "px";
-
-}, 1000);
-
-function shoot(zombie){
-    alert("GOT CHA");
+	if(scorePoints < 1) {
+		alert("DEAD BRAIN");
+		window.location.reload();
+	}
 }
 
 
 
-let score = 0;
 
 
-button.addEventListener('click',() => {
-gameFrame.appendChild(zombie);
-});
+
+
+
+
+const zombie1 = document.getElementById("zombie1");
+const zombie2 = document.getElementById("zombie2");
+const zombie3 = document.getElementById("zombie3");
+const zombie4 = document.getElementById("zombie4");
+
+const zombies = [zombie1,zombie2,zombie3,zombie4];
+
+for(let i = 0;i<zombies.length;i++){
+	
+	zombies[i].addEventListener('click',()=>{
+		shoot(zombies[i]);
+		zombies[i + 1].style.display = "block";
+	})
+	
+}
+
+
+
+
+
+
+
+function livingZombie() {
+	return document.querySelectorAll(".zombie:not(.dead)");
+}
+
+
+function shoot(zombie) {
+	zombie.classList.add("dead");
+
+	if(!livingZombie().length) {
+		alert("You BLASTED EM'!");
+		window.location.reload();
+	}
+
+}
+
+
+function zombieAttacksMe(zombie) {
+
+	if(scorePoints > 0) {
+
+		zombie.classList.add("showing");
+
+		setTimeout(()=> {
+			zombieShootsMe(zombie);
+		}, 1000);
+
+		setTimeout(()=> {
+			zombie.classList.remove("showing");
+		}, 3000);
+		
+	}
+
+
+}
+
+
+function zombieShootsMe(zombie) {
+
+	if(!zombie.classList.contains("dead")) {
+
+		zombie.classList.add("shooting");
+		updateScorePoints(scorePoints - 20);
+
+		setTimeout(()=> {
+			zombie.classList.remove("shooting");
+		}, 200);
+
+	}
+
+}
+
+
+function randomZombieAttacks() {
+
+	var randomZombieyNo = Math.random() * livingZombie().length;
+	randomZombieNo = Math.floor(randomZombieNo);
+	var zombie = livingZombie()[randomZombieNo];
+
+	var randomDelay = Math.random() * 2000 + 1000;
+
+	setTimeout( ()=> {
+		zombieAttacksMe(zombie);
+		randomZombieAttacks();
+	}, randomDelay);
+
+}
+
+//button
+const startButton = document.getElementById("startButton")
+startButton.addEventListener('click',()=>{
+	zombie1.style.display = "block";
+})
