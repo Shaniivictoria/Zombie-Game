78a9bb05ac3201fc7468a79e5565dd3d21a6f5ff
Shani Victoria <122415617+Shaniivictoria@users.noreply.github.com>
2023-02-27 13:23:16 -0500

    Removed some functions

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-var healthPoints = 100;
-
-function updateScoreBar(points) {
-
-	scoorePoints = points;
-	var scoreBar = document.querySelector(".scoreBar");
-	scoreBar.style.width = points + "%";
-
-	if(scorePoints < 1) {
-		alert("DEAD BRAIN");
-		window.location.reload();
-	}
-}
-
-
-
-
-
-
-
-
 
 const zombie1 = document.getElementById("zombie1");
 const zombie2 = document.getElementById("zombie2");
@@ -35,10 +14,6 @@ for(let i = 0;i<zombies.length;i++){
 	})
 	
 }
-
-
-
-
 
 
 
@@ -94,20 +69,6 @@ function zombieShootsMe(zombie) {
 }
 
 
-function randomZombieAttacks() {
-
-	var randomZombieyNo = Math.random() * livingZombie().length;
-	randomZombieNo = Math.floor(randomZombieNo);
-	var zombie = livingZombie()[randomZombieNo];
-
-	var randomDelay = Math.random() * 2000 + 1000;
-
-	setTimeout( ()=> {
-		zombieAttacksMe(zombie);
-		randomZombieAttacks();
-	}, randomDelay);
-
-}
 
 //button
 const startButton = document.getElementById("startButton")
